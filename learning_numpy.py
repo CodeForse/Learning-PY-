a3474923c5348b46b9ef892ee7dd352f35b028d1
Aslan <45170182+CodeForse@users.noreply.github.com>
2022-06-06 19:45:03 +0600

    2 graphs, obtaining statistic in the next files

--- learning_numpy.py
+++ learning_numpy.py
@@ -3,17 +3,32 @@ import numpy as np
 import math 
 import matplotlib.pyplot as plt
 
+#np is easy, it more like plt and preparing to analyze the data
 dataset1=np.array([1,2,3])
-dataset2=np.array([1,5,10])
-means_arr=np.array(np.mean(dataset1),dataset2.mean())
-sem_arr=np.array(np.std(dataset1,ddof=1)/math.sqrt(len(dataset1)),dataset2.std(ddof=1)/math.sqrt(dataset2.size))
+dataset2=np.array([1,3,5,8])
+
+means_arr=np.array([np.mean(dataset1),dataset2.mean()])
+sem_arr=np.array([np.std(dataset1,ddof=1)/math.sqrt(len(dataset1)),dataset2.std(ddof=1)/math.sqrt(dataset2.size)])
 plt.bar(x=['dataset1','dataset2'],height=means_arr, yerr=sem_arr,capsize=4)
 plt.show()
 
+#class LinRegTwoVars:
+#    Xarr=[0]
+#    Yarr=[0]
+#    
+#    def __init__(x,y): 
+#        LinRegTwoVars.Xarr=x
+#        LinRegTwoVars.Yarr=y
+#        
+#    r=sum((Xarr-np.average(Xarr))*(Yarr-np.average(Yarr)))/math.sqrt(sum(((Xarr-np.average(Xarr))**2))*sum((Yarr-np.mean(Yarr.mean))**2))
+    
+# model=LinRegTwoVars(np.array([1,2,3]),np.array([6,5,4]))
+# print(model.r) back to this later
+
 def PERT(t_lik,t_pes,t_opt,itterations):
     mu=(t_lik*4+t_pes+t_opt)/6
-    mu=(mu-t_lik)/(t_pes-t_opt)
-    sigma=math.sqrt((t_pes-t_opt)**2/36)
+    mu=(mu-t_opt)/(t_pes-t_opt)
+    sigma=math.sqrt(((t_pes-t_opt)**2)/36)
     sigma/=(t_pes-t_opt)
     chi=mu*(1-mu)/sigma**2-1
     alfa=mu*chi
@@ -29,3 +44,5 @@ plt.show()
  
 
 print('probability that duration will be <=20: '+str(sum(1 for x in freq_PERT if x<=20)/int(1e6)))
+
+#therefore we've obtained 2 graphs
