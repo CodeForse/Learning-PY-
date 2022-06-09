a0a699a37a1880dbd5bad0929cd1bcfc3813aebd
Aslan <45170182+CodeForse@users.noreply.github.com>
2022-06-09 13:01:29 +0600

    spicy stat T test ez

--- analyze_data.py
+++ analyze_data.py
@@ -1,25 +1,13 @@
 
 
+from operator import le
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import math 
-#import spicy.stats as st
+import scipy.stats as st #from spicy import stats as st
 
 
-# data=pd.read_excel('D:/Users/Аслан/Desktop/программирование/PythonDanila/L&L project.xlsx',sheet_name='Лист3', index_col=0)
-# #print(data)
-# #print(data.to_dict())
-# Xarr=np.array(data['Time']) #data.Time or data[0]
-# Yarr=np.array(data['Индекс реальных денежных доходов, %'])
-# n=len(Xarr)
-
-
-# Xavr=np.average(Xarr)
-# Yavr=Yarr.mean()
-# r=np.sum((Xarr-Xavr)*(Yarr-Yavr))/np.sqrt(np.sum((Xarr-Xavr)**2)*np.sum((Yarr-Yavr)**2))
-# #t_test=r*math.sqrt((n-2)/(1-r*r))
-# #print('r=',round(r,3))
 
 class AnalyzeLinkTwoVars:
    
@@ -29,7 +17,6 @@ class AnalyzeLinkTwoVars:
 
     #@staticmethod
     def getRegressionCoef(self,x,y):
-        print(self.data[(y)])
 
         Yarr=np.array(self.data[y])
         Xarr=np.array(self.data[x])
@@ -38,15 +25,20 @@ class AnalyzeLinkTwoVars:
         r=np.sum((Xarr-Xavr)*(Yarr-Yavr))/np.sqrt(np.sum((Xarr-Xavr)**2)*np.sum((Yarr-Yavr)**2))
         return r
     def StudentTest(self,x,y,SignLevel):
+        #H0: in the model Y=bX+a, b=0
+        #H1=HA: b!=0
         r=self.getRegressionCoef(x,y)
         t_test=r*math.sqrt((len(x)-2)/(1-r*r))
-        return t_test
-        #not working well as spicy stat is no available , df=n-1 and a=Sign level
+        t_crit=st.t.ppf(q=SignLevel,df=len(x)-2)
+        return t_test<t_crit #0->accepth null, 1-> reject null
+        #(solved)not working well as spicy stat is no available , df=n-2 and a=Sign level: damn it was scipy
+
          
 
-
-model=AnalyzeLinkTwoVars('D:/Users/Аслан/Desktop/программирование/PythonDanila/L&L project.xlsx','Лист3')
-print(model.getRegressionCoef('Time','Индекс реальных денежных доходов, %'))        
-
+path=''
+model=AnalyzeLinkTwoVars(path,'Лист3')
+print(model.getRegressionCoef('Time','Индекс реальных денежных доходов, %'))  
+SL=0.05      
+print('Thy hypothesis that Time does not affects Index of the real income is '+str(model.StudentTest('Time','Индекс реальных денежных доходов, %',SL))+' at '+str(SL)+'SL')
         
        
