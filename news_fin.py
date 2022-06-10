5ca0bda7268df2ebf991c48b3e64b48efdd7eb22
Aslan <45170182+CodeForse@users.noreply.github.com>
2022-06-11 01:05:49 +0600

    news are adding to the file, placed in the same place were the PY file

--- news_fin.py
+++ news_fin.py
@@ -6,6 +6,8 @@ first_page=1
 border_page=5
 sample_size=4
 
+file = open('news.txt', 'w')
+file.truncate()
 total_for_range = []
 for p in range(first_page,border_page):
      url=f"https://tengrinews.kz/kazakhstan_news/page/{p}"
@@ -19,3 +21,6 @@ for i in random.sample(total_for_range,sample_size):
   print(str(k)+') ')
   k=k+1
   print(i)
+  file.write(i+ '\n')
+
+file.close()
